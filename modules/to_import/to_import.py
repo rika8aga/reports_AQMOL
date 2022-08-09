a9cd73f8fac1f8e07a15bf3067e18b261f87b6eb
rika8aga <zharmakin@liva.com>
2022-08-09 12:15:40 +0600

    Version 1.6

--- modules/to_import/to_import.py
+++ modules/to_import/to_import.py
@@ -7,7 +7,7 @@ import locale
 from modules.levels import Groups
 from modules.plots import value_format, float_format
 
-locale.setlocale(locale.LC_ALL, "ru_RU.utf8")
+locale.setlocale(locale.LC_ALL, "ru")
 
 
 @st.experimental_memo(show_spinner=False)
